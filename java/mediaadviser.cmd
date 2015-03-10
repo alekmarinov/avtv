cbfc3199dbef584a5b63eace4c1867654d27a55e
Alexander Marinov <alek.marinov@aviq.bg>
2015-03-10 15:54:24 +0200

    Build MediaAdviser project

--- java/mediaadviser.cmd
+++ java/mediaadviser.cmd
@@ -1,2 +1,3 @@
 @echo off
-java -cp lib/mahout-core-0.8.jar;lib/mahout-core-0.8-job.jar;lib/mahout-integration-0.8.jar;lib/mahout-math-0.8.jar;lib/commons-pool2-2.0.jar;lib/jedis-2.4.2.jar;bin intelibo.avtv.recommender.MediaAdviser localhost 6379
+call ant jar
+java -cp lib/mahout-core-0.8.jar;lib/mahout-core-0.8-job.jar;lib/mahout-integration-0.8.jar;lib/mahout-math-0.8.jar;lib/commons-pool2-2.0.jar;lib/jedis-2.4.2.jar;MediaAdviser.jar intelibo.avtv.recommender.MediaAdviser localhost 6379
